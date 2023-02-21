4ccaa7d8f86c9fd00ec9f51fccbe094bc771399f
Ive <ive.alex.89@gmail.com>
2023-02-20 19:50:07 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Mainclass { 
+static void Main(string[] args)
+{
+    string MyName;
+    MyName = "Ive";
+    Console.WriteLine(MyName);
+    Console.ReadKey();
+}
+}
