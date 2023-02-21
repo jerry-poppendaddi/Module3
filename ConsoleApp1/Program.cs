a2ba13d045007c8b6f3c93544bb357fbd36b6229
Ive <ive.alex.89@gmail.com>
2023-02-20 20:51:45 -0500

    More Commands used

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,13 @@ static void Main(string[] args)
 {
     string MyName;
     MyName = "Ive";
-    Console.WriteLine(MyName);
+        bool Pet = true;
+    ushort age = 33;
+    double shoesize = 10.5;
+    Console.WriteLine("My name is {0}",MyName);
+        Console.WriteLine("My age is {0}", age);
+        Console.WriteLine("Do I Have A Pet?" + Pet);
+        Console.WriteLine("My shoesize is {0}", shoesize);
     Console.ReadKey();
 }
 }
